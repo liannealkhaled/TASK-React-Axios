6cc2f7337b772bf47f8ec82cfb03ef946e473011
lianne <liannealq@hotmail.com>
2023-10-10 20:03:34 +0300

    isloading?

--- src/components/PetDetail.js
+++ src/components/PetDetail.js
@@ -53,15 +53,15 @@ const PetDetail = () => {
       <div className="border border-black rounded-md w-[70%] h-[70%] overflow-hidden flex flex-col md:flex-row p-5">
         <div className="h-full w-full md:w-[35%]">
           <img
-            src={pet.image}
-            alt={pet.name}
+            src={pet?.image}
+            alt={pet?.name}
             className="object-contain w-full h-full"
           />
         </div>
         <div className="w-full md:w-[65%] h-full pt-[30px] flex flex-col p-3">
-          <h1>Name: {pet.name}</h1>
-          <h1>Type: {pet.type}</h1>
-          <h1>adopted: {pet.adopted}</h1>
+          <h1>Name: {pet?.name}</h1>
+          <h1>Type: {pet?.type}</h1>
+          <h1>adopted: {pet?.adopted}</h1>
 
           <button className="w-[70px] border border-black rounded-md  hover:bg-green-400 mb-5">
             Adobt
